66ac6d3200c7cd8715b0333de68440288010c4da
LucasDegenhardtSeltmann <lucas.degenhardt-seltmann@hs-bochum.de>
2020-11-03 15:29:46 +0100

    Refactor on MVC Model, Csv Sxport

--- AnwendungFreizeitbaeder_Vorgabe/src/gui/FreizeitbaederControl.java
+++ AnwendungFreizeitbaeder_Vorgabe/src/gui/FreizeitbaederControl.java
@@ -1,5 +1,55 @@
 package gui;
 
+import java.io.IOException;
+
+import business.FreizeitbaederModel;
+import javafx.scene.control.TextArea;
+import ownUtil.PlausiException;
+
 public class FreizeitbaederControl {
+	private FreizeitbaederView freizeitbaederView;
+	private FreizeitbaederModel freizeitbad;
+	
+	public FreizeitbaederControl(FreizeitbaederView fbv) {
+		freizeitbaederView = fbv;
+	}
+	public void nehmeFreizeitbadAuf(String name, String von, String bis, String laenge, String temp){
+    	try{
+    		this.freizeitbad = new FreizeitbaederModel(name, von, bis, laenge, temp);
+    		freizeitbaederView.zeigeInformationsfensterAn("Das Freizeitbad wurde aufgenommen!");
+       	}
+       	catch(PlausiException exc){
+       		freizeitbaederView.zeigeFehlermeldungsfensterAn(exc.getPlausiTyp() + "er ", exc.getMessage());
+     	}
+    }
+	public void zeigeFreizeitbaederAn(TextArea text){
+    	if(this.freizeitbad != null){
+    		text.setText(
+    			this.freizeitbad.gibFreizeitbadZurueck(' '));
+    	}
+    	else{
+    		freizeitbaederView.zeigeInformationsfensterAn("Bisher wurde kein Freizeitbad aufgenommen!");
+    	}
+    }
+	void schreibeFreizeitbaederInDatei(String typ){
+		 try{
+			 if("csv".equals(typ)){
+				 freizeitbad.schreibeFreizeitbaederInCsvDatei();
+				 freizeitbaederView.zeigeInformationsfensterAn("Die Freizeitbäder wurden gespeichert!");
+			 }
+			 else{
+				 freizeitbaederView.zeigeInformationsfensterAn("Noch nicht implementiert!");
+			 }
+		 }
+		catch(IOException exc){
+			 freizeitbaederView.zeigeFehlermeldungsfensterAn("IOException","IOException beim Speichern!");
+		}
+		 catch(Exception exc){
+				freizeitbaederView.zeigeFehlermeldungsfensterAn("unknown","Unbekannter Fehler beim Speichern!");
+		}
+	}
+		 
+	
+		
 
 }
--- AnwendungFreizeitbaeder_Vorgabe/src/main/Main.java
+++ AnwendungFreizeitbaeder_Vorgabe/src/main/Main.java
@@ -1,6 +1,6 @@
 package main;
 
-import gui.FreizeitbaederAnwendersystem;
+import gui.FreizeitbaederView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -8,7 +8,7 @@ public class Main extends Application {
 	
 	@Override
 	public void start(Stage primaryStage) {
-		new FreizeitbaederAnwendersystem(primaryStage);
+		new FreizeitbaederView(primaryStage);
 	}	
 	
 	public static void main(String[] args){
